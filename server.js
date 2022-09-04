019ac59db9b68fdca62d3cd6280497a003f4637c
Leksyking <gbemilekeogundipe@gmail.com>
2022-09-04 20:08:33 +0100

    add an event listener for connection

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io")
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder
 app.use(express.static("public"));
 
+//Run when client connects
+io.on("connection", socket => {
+    console.log("New WS connection");
+})
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port, () => console.log(`Server running on port ${port}`));
