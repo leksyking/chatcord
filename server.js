5a66228c41cddf6b541658a5d9e2bbc13de8e72b
Leksyking <gbemilekeogundipe@gmail.com>
2022-09-04 21:05:45 +0100

    handle series of brosdcast events on the server

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ app.use(express.static("public"));
 //Run when client connects
 io.on("connection", socket => {
     console.log("New WS connection");
+
+    //emits to the client making the connection
+    socket.emit("message", "Welcome to ChatCord");
+
+    //broadcast when a client connects
+    //emits to everybody except the user
+    socket.broadcast.emit("message", "A user has joined the chat");
+
+    //Runs when client disconnects
+    socket.on("disconnect", () => {
+        //to all clients in the room
+        io.emit("message", "A user has left the chat");
+    });
 })
 
 const port = process.env.PORT || 3000;
